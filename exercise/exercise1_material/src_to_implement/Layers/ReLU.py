18f1fb431f652ceef22e58ae9064883416626669
youyizhang <aaron.zhang.edu@outlook.com>
2022-11-16 08:06:19 +0800

    add comment

--- exercise/exercise1_material/src_to_implement/Layers/ReLU.py
+++ exercise/exercise1_material/src_to_implement/Layers/ReLU.py
@@ -7,9 +7,15 @@ class ReLU(BaseLayer):
         super().__init__()
         self._x = None
 
+    '''
+    x = x > 0 ? x : 0
+    '''
     def forward(self, input_tensor):
         self._x = 1 * (input_tensor > 0) * input_tensor
         return self._x
 
+    '''
+    En = x > 0 ? En : 0
+    '''
     def backward(self, error_tensor):
         return 1 * (self._x > 0) * error_tensor
--- exercise/exercise1_material/src_to_implement/Optimization/Loss.py
+++ exercise/exercise1_material/src_to_implement/Optimization/Loss.py
@@ -1,13 +1,19 @@
 import numpy as np
 
 
-class CrossEntropyLoss():
+class CrossEntropyLoss:
     def __init__(self):
         self._y = None
 
+    '''
+    loss = -∑ln(y_hat + ε)
+    '''
     def forward(self, prediction_tensor, label_tensor):
         self._y = prediction_tensor
         return -np.sum(label_tensor * np.log(self._y + np.finfo(float).eps))
 
+    '''
+    En = -y / (y_hat + ε)
+    '''
     def backward(self, label_tensor):
-        return -(label_tensor / (self._y + np.finfo(float).eps))
+        return - (label_tensor / (self._y + np.finfo(float).eps))
