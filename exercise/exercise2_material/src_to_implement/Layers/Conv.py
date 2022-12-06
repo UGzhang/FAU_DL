e7d4e9412344b9c64827803124832087a7717ba0
youyizhang <aaron.zhang.edu@outlook.com>
2022-12-06 23:26:47 +0800

    bug in "def test_backward_size"

--- exercise/exercise2_material/src_to_implement/Layers/Conv.py
+++ exercise/exercise2_material/src_to_implement/Layers/Conv.py
@@ -1,78 +1,45 @@
 import numpy as np
-import math
+import math, copy
 from Layers.Base import BaseLayer, Tool
 
 
 class Conv(BaseLayer):
+
     def __init__(self, stride_shape, convolution_shape, num_kernels):
         super().__init__()
         self.trainable = True
-        # get stride at row and col direction
+
         self.stri_h = stride_shape[0]
         self.stri_w = stride_shape[1] if type(stride_shape) == tuple else 1
 
-        # add a third dimension 1 to Conv_shape
-        if len(convolution_shape) == 2:
-            convolution_shape = convolution_shape + (1,)
+        if len(convolution_shape) == 2: convolution_shape = convolution_shape + (1,)
 
-        # insert filter batch in to weight shape; weight_shape(kern_num, channel, height, width)
         self.weight_shape = (num_kernels,) + convolution_shape
 
-        # print(self.weight_shape)
+        self.convolution_shape = convolution_shape
 
-        self.weights = np.random.uniform(0, 1, size=self.weight_shape)
-        self.kern_num = num_kernels
-        self.bias = np.random.uniform(0, 1, size=num_kernels)
+        self.weights = np.random.uniform(size=(num_kernels, *convolution_shape))  # 0-1
+        self.bias = np.random.uniform(size=num_kernels)
+
         # self.gradient_weights = None
         # self.gradient_bias = None
 
-    """
-    optimizer for weights and bias will be separate
-    """
+        self._input_shape = None
+        self.num_ker = num_kernels
+        self._optimizer = None
+        self._Conv1D = False
 
-    @property
-    def optimizer(self):
-        return self._optimizer
-
-    @optimizer.setter
-    def optimizer(self, Obj):
-        self._optimizer = Obj
-        self._optimizer_b = copy.deepcopy(self._optimizer)
-        self._optimizer_w = copy.deepcopy(self._optimizer)
-
-    def im2col(self, input, f_h, f_w, out_h, out_w):
-        # input_tensor.shape = (N, C, H, W)
-        # input_col.shape = (out_h * out_w * batch, channels * filter_h * filter_w )
-        N, C, H, W = input.shape
-
-        col = np.zeros((N, C, f_h, f_w, out_h, out_w))
-
-        for y in range(f_h):
-            y_max = y + self.stri_h * out_h
-            for x in range(f_w):
-                x_max = x + self.stri_w * out_w
-                col[:, :, y, x, :, :] = input[:, :, y:y_max:self.stri_h, x:x_max:self.stri_w]
-        col = col.transpose(0, 4, 5, 1, 2, 3).reshape(N * out_h * out_w, -1)
-        return col
-
-    """
-    Transpos Variables in forward function
-    self.input_shape_origin : origin shape of input_tensor
-    self.input_shape : shape of input_tensor after padding
-    self.out_shape : shape of output
-    """
-
+    '''
+        input_tensor: [batch,channel,height,width]
+    '''
     def forward(self, input_tensor):
 
-        # origin input shape
-        self.input_shape_origin = input_tensor.shape
-        # add a width dimension to 3D input_tensor
         if len(input_tensor.shape) == 3:
-            input_tensor = input_tensor[..., np.newaxis]
+            self._Conv1D = True
+            input_tensor = np.expand_dims(input_tensor, axis=3)
 
-        # extended input shape
-        self.input_shape_extend = input_tensor.shape
-
+        self._input_shape = input_tensor.shape
+        N, C, H, W = input_tensor.shape
         num, channels, filter_h, filter_w = self.weights.shape
 
         # SAME padding
@@ -80,123 +47,56 @@ class Conv(BaseLayer):
         pad_h2 = int(math.ceil((filter_h - 1) / 2))
         pad_w1 = int(math.floor((filter_w - 1) / 2))
         pad_w2 = int(math.ceil((filter_w - 1) / 2))
-        input_tensor = np.pad(input_tensor, ((0, 0), (0, 0), (pad_h1, pad_h2), (pad_w1, pad_w2)),
-                              'constant', constant_values=0)
+        input_tensor = np.pad(input_tensor, ((0, 0), (0, 0), (pad_h1, pad_h2), (pad_w1, pad_w2)), 'constant', constant_values=0)
 
         batch, channels, height, width = input_tensor.shape
 
-        # padded input shape
         self.input_shape = input_tensor.shape
         self.input_channel = channels
 
-        # cal output shape first; height and weight already padded
         out_h = int((height - filter_h) / self.stri_h) + 1
         out_w = int((width - filter_w) / self.stri_w) + 1
 
-        # transform the input_tensor to a large 2D matrix
-        self.input_col = self.im2col(input_tensor, filter_h, filter_w, out_h, out_w)
+        self.input_col = Tool().im2col(input_tensor, [filter_h, filter_w], [self.stri_h, self.stri_w])
+        self.weight_col = self.weights.reshape(self.num_ker, -1).T
 
-        # tranform filter to a matrix; transit to multiply
-        self.weight_col = self.weights.reshape(self.kern_num, -1).T
+        out = np.dot(self.input_col, self.weight_col) + self.bias
 
-        # add bias to the end of weight_col
-        weight_col_bias = np.vstack((self.weight_col, self.bias))
+        output = out.reshape(batch, out_h, out_w, -1).transpose(0, 3, 1, 2)
+        self.output = output
 
-        # add one col 1 to the end of input_col
-        bias_input = np.ones((self.input_col.shape[0], 1))
-        input_col_plus = np.c_[self.input_col, bias_input]
-
-        output = np.dot(input_col_plus, weight_col_bias)
-        output = output.reshape(batch, out_h, out_w, -1).transpose(0, 3, 1, 2)
-        self.out_shape = output.shape
-
-        # if input_tensor is 3D, then change output back to 3D
-        if len(self.input_shape_origin) == 3:
-            output = np.squeeze(output, axis=3)
-
-        return output
-
-    """
-    Transfer 2D back to 4D 
-    """
-
-    def col2im(self, col, input_shape, filter_h, filter_w):
-
-        N, C, H, W = input_shape
-
-        pad_h1 = int(math.floor((filter_h - 1) / 2))
-        pad_h2 = int(math.ceil((filter_h - 1) / 2))
-        pad_w1 = int(math.floor((filter_w - 1) / 2))
-        pad_w2 = int(math.ceil((filter_w - 1) / 2))
-
-        out_h = (H + pad_h1 + pad_h2 - filter_h) // self.stri_h + 1
-        out_w = (W + pad_w1 + pad_w2 - filter_w) // self.stri_w + 1
-
-        # print("reshape = ", N, ",",out_h, ",",out_w, ",",C, ",",filter_h, ",",filter_w)
-
-        col = col.reshape(N, out_h, out_w, C, filter_h, filter_w).transpose(0, 3, 4, 5, 1, 2)
-
-        # add the padding to image
-        img = np.zeros((N, C, H + pad_h1 + pad_h2 + self.stri_h - 1, W + pad_w1 + pad_w2 + self.stri_w - 1))
-
-        for y in range(filter_h):
-            y_max = y + self.stri_h * out_h
-            for x in range(filter_w):
-                x_max = x + self.stri_w * out_w
-                img[:, :, y:y_max:self.stri_h, x:x_max:self.stri_w] += col[:, :, y, x, :, :]
-
-        # remove the pad from output
-        return img[:, :, pad_h1:H + pad_h1, pad_w1:W + pad_w1]
+        if self._Conv1D:
+            self.output = np.squeeze(self.output, axis=3)
+        return self.output
 
     def backward(self, error_tensor):
+        error_tensor = error_tensor.transpose(0, 2, 3, 1).reshape(-1, self.num_ker)
 
-        self.err_shape_origin = error_tensor.shape
-
-        # add a x axis for 3D error_tensor
-        if len(error_tensor.shape) == 3:
-            error_tensor = error_tensor[..., np.newaxis]
-
-        # error_tensor.shape(batch, kern_num, out_h, out_w)
-        self.err = error_tensor
-
-        # col_err.shape(out_h*out_w*batch , kern_num)
-        col_err = error_tensor.transpose(0, 2, 3, 1).reshape(-1, self.kern_num)
-        # cal gradient of weights and bias
-        self.gradient_weights = np.dot(self.input_col.T, col_err)
+        self.gradient_weights = np.dot(self.input_col.T, error_tensor)
         self.gradient_weights = self.gradient_weights.transpose(1, 0).reshape(self.weights.shape)
-        self.gradient_bias = np.sum(col_err, axis=0)
-
-        # cal grad of input in col form
-        d_input_col = np.dot(col_err, self.weight_col.T)
-
-        # transform col form back to image form
+        self.gradient_bias = np.sum(error_tensor, axis=0)
         num, channels, filter_h, filter_w = self.weights.shape
-        next_err = self.col2im(d_input_col, self.input_shape_extend, filter_h, filter_w)
 
-        # update parameters in optimizers
-        if self._optimizer_w:
-            self.weights = self._optimizer_w.calculate_update(self.weights, self.gradient_weights)
-        if self._optimizer_b:
-            self.bias = self._optimizer_b.calculate_update(self.bias, self.gradient_bias)
+        dcol = np.dot(error_tensor, self.weight_col.T)
+        dx = Tool().col2im(dcol, self._input_shape, [filter_h,filter_w], [self.stri_h, self.stri_w])
 
-        if len(self.err_shape_origin) == 3:
-            next_err = np.squeeze(next_err, axis=3)
+        if self._optimizer is not None:
+            self.weights = self._optimizer.calculate_update(self.weights, self.gradient_weights)
+            self.bias = self._optimizer.calculate_update(self.bias, self.gradient_bias)
+        return dx
 
-        return next_err
+    def initialize(self, weights_initializer, bias_initializer):
+        self.weights = weights_initializer.initialize(self.weights.shape, np.prod(self.convolution_shape),
+                                                      np.prod(self.convolution_shape[1:]) * self.num_kernels)
+        self.bias = bias_initializer.initialize(self.bias.shape, 1, self.num_kernels)
 
-    @property
-    def gradient_weights(self):
-        return self.gradient_weights
-
-    @gradient_weights.setter
-    def gradient_weights(self, value):
-        self.gradient_weights = value
 
     @property
-    def gradient_bias(self):
-        return self.gradient_bias
+    def optimizer(self):
+        return self._optimizer
 
-    @gradient_bias.setter
-    def gradient_bias(self, value):
-        self.gradient_bias = value
-
+    @optimizer.setter
+    def optimizer(self, _opt):
+        self._optimizer = _opt
+        self._optimizer.weights = copy.deepcopy(_opt)
+        self._optimizer.bias = copy.deepcopy(_opt)
--- exercise/exercise2_material/src_to_implement/Layers/FullyConnected.py
+++ exercise/exercise2_material/src_to_implement/Layers/FullyConnected.py
@@ -12,39 +12,39 @@ class FullyConnected(BaseLayer):
         self.weights = np.vstack((self.weight, self.bias))  # cols concat
 
         self.gradient_weights = None
-        self.__optimizer = None
-        self.__x = None
+        self._optimizer = None
+        self._x = None
 
         # add in ex2
-        self.__input_size = input_size
-        self.__output_size = output_size
+        self._input_size = input_size
+        self._output_size = output_size
 
     '''
     y_hat = X * W
     '''
     def forward(self, input_tensor):
         # x concat cols of 1
-        self.__x = np.c_[input_tensor, np.ones(input_tensor.shape[0])]
-        return np.matmul(self.__x, self.weights)
+        self._x = np.c_[input_tensor, np.ones(input_tensor.shape[0])]
+        return np.matmul(self._x, self.weights)
 
     '''
     En-1 = En * W.T
     '''
     def backward(self, error_tensor):
-        self.gradient_weights = np.matmul(self.__x.T, error_tensor)
-        if self.__optimizer is not None:
-            self.weights = self.__optimizer.calculate_update(self.weights, self.gradient_weights)
+        self.gradient_weights = np.matmul(self._x.T, error_tensor)
+        if self._optimizer is not None:
+            self.weights = self._optimizer.calculate_update(self.weights, self.gradient_weights)
         return np.dot(error_tensor, self.weight.T)
 
     @property
     def optimizer(self):
-        return self.__optimizer
+        return self._optimizer
 
     @optimizer.setter
     def optimizer(self, _opt):
-        self.__optimizer = _opt
+        self._optimizer = _opt
 
     # add in ex2
     def initialize(self, weights_initializer, bias_initializer):
-        self.weight = weights_initializer.initialize(self.weights.shape, self.__input_size, self.__output_size)
-        self.bias = bias_initializer.initialize(self.weights.shape, self.__input_size, self.__output_size)
+        self.weight = weights_initializer.initialize(self.weights.shape, self._input_size, self._output_size)
+        self.bias = bias_initializer.initialize(self.weights.shape, self._input_size, self._output_size)
