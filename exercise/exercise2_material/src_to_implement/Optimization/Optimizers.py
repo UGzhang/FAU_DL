1f97f2bba6511d45ea5040e5df1fbefced30870d
youyizhang <aaron.zhang.edu@outlook.com>
2022-12-13 22:53:21 +0800

    reduce some lines

--- exercise/exercise2_material/src_to_implement/Optimization/Optimizers.py
+++ exercise/exercise2_material/src_to_implement/Optimization/Optimizers.py
@@ -3,7 +3,7 @@ import math
 
 
 class Sgd:
-    def __init__(self, lr:float):
+    def __init__(self, lr: float):
         self.lr = lr
 
     def calculate_update(self, weight_tensor, gradient_tensor):
@@ -13,36 +13,30 @@ class Sgd:
 class SgdWithMomentum:
 
     def __init__(self, lr, mr):
-        self._learning_rate = lr
-        self._momentum_rate = mr
+        self._lr = lr
+        self._mr = mr  # momentum rate
         self._v = 0
 
     def calculate_update(self, weight_tensor, gradient_tensor):
-        self._v = self._momentum_rate * self._v - self._learning_rate * gradient_tensor
+        self._v = self._mr * self._v - self._lr * gradient_tensor
         return weight_tensor + self._v
 
 
 class Adam:
 
-    def __init__(self, learing_rate, mu, rho):
-        self._lr = learing_rate
+    def __init__(self, lr, mu, rho):
+        self._lr = lr
         self._mu = mu
         self._rho = rho
         self._v = 0
         self._r = 0
         self._k = 1
 
-    # choose iter_num = 1 at beginning
     def calculate_update(self, weight_tensor, gradient_tensor):
-        # first momentum
         self._v = self._mu * self._v + (1 - self._mu) * gradient_tensor
-        # second momentum (squared gradient)
         self._r = self._rho * self._r + (1 - self._rho) * gradient_tensor * gradient_tensor
-        # bias correction
         v_hat = self._v / (1 - self._mu ** self._k)
         r_hat = self._r / (1 - self._rho ** self._k)
         w = weight_tensor - self._lr * (v_hat / (np.sqrt(r_hat) + np.finfo(float).eps))
-        # change iter_num = 2 for next iteration
         self._k += 1
         return w
-
