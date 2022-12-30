c21d13635a4c8cd919ffd80de025cec56eace33b
youyizhang <aaron.zhang.edu@outlook.com>
2022-12-30 21:38:00 +0800

    update RNN

--- exercise3_material/src_to_implement/Layers/RNN.py
+++ exercise3_material/src_to_implement/Layers/RNN.py
@@ -9,34 +9,41 @@ class RNN(BaseLayer):
         self.trainable = True
 
         self.input_size = input_size
-        self.hidden_state = hidden_size
+        self.hidden_size = hidden_size
         self.output_size = output_size
 
         self.hidden_state = np.zeros(hidden_size)
         self.memorize = False
 
-        self.weights = None
-        self.bias = None
+        self.W_y = np.random.uniform(size=(hidden_size, output_size))
+        self.B_y = np.random.uniform(size=output_size)
+        self.W_hx = np.random.uniform(size=(input_size + hidden_size, hidden_size))
+        self.B_hx = np.random.uniform(size=hidden_size)
 
         self._optimizer = None
 
     def forward(self, input_tensor):
-        if self.memorize:
-            pass
-        else:
-            pass
+        batch_size = input_tensor.shape[0]
+        output_tensor = np.zeros((batch_size, self.hidden_size))
+        if self.memorize is False:
+            self.hidden_state = np.zeros(self.hidden_size)
+        for b in range(batch_size):
+            x_hat = np.hstack((self.hidden_state, input_tensor[b]))
+            self.hidden_state = np.tanh(np.dpt(x_hat, self.W_hx) + self.B_hx)  # ht
+            y = np.dot(self.hidden_state, self.W_y) + self.B_y
+            output_tensor[b] = 1 / (1 + np.exp(-y))
 
-    @property
-    def memorize(self):
-        return self.memorize
-
-    @memorize.setter
-    def memorize(self, x):
-        self.memorize = x
+        return output_tensor
 
     def initialize(self, weights_initializer, bias_initializer):
-        self.weights = weights_initializer.initialize(self.input_size[0])
-        self.bias = bias_initializer.initialize(self.input_size[0])
+        self.W_y = weights_initializer.initialize(self.W_y.shape, self.hidden_size, self.output_size)
+        self.B_y = bias_initializer.initialize(self.B_y.shape, self.hidden_size, self.output_size)
+        self.W_hx = weights_initializer.initialize(self.W_hx.shape, self.W_hx.shape[0], self.hidden_size)
+        self.B_hx = bias_initializer.initialize(self.B_hx.shape, self.W_hx.shape[0], self.hidden_size)
+
+    @property
+    def calculate_regularization_loss(self):
+        return self.optimizer.regularizer.norm(self.)
 
     @property
     def optimizer(self):
@@ -45,5 +52,13 @@ class RNN(BaseLayer):
     @optimizer.setter
     def optimizer(self, _opt):
         self._optimizer = _opt
-        self._optimizer.weights = copy.deepcopy(_opt)
-        self._optimizer.bias = copy.deepcopy(_opt)
+        # self._optimizer.weights = copy.deepcopy(_opt)
+        # self._optimizer.bias = copy.deepcopy(_opt)
+
+    @property
+    def memorize(self):
+        return self.memorize
+
+    @memorize.setter
+    def memorize(self, x):
+        self.memorize = x
