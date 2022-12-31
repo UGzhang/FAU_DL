20fa94b24ff755504e0981a3d8d86ca1497e88ed
youyizhang <aaron.zhang.edu@outlook.com>
2022-12-31 22:54:18 +0800

    update RNN

--- exercise3_material/src_to_implement/Layers/RNN.py
+++ exercise3_material/src_to_implement/Layers/RNN.py
@@ -1,64 +1,29 @@
 import numpy as np
-import copy
 from Layers.Base import BaseLayer
+from Layers.FullyConnected import FullyConnected
+from Layers.TanH import TanH
 
 
 class RNN(BaseLayer):
     def __init__(self, input_size, hidden_size, output_size):
         super().__init__()
         self.trainable = True
+        self._memorize = False
 
         self.input_size = input_size
         self.hidden_size = hidden_size
         self.output_size = output_size
 
-        self.hidden_state = np.zeros(hidden_size)
-        self.memorize = False
+        self.FC_h = FullyConnected(input_size+hidden_size, hidden_size)
+        self.FC_y = FullyConnected(hidden_size, output_size)
 
-        self.W_y = np.random.uniform(size=(hidden_size, output_size))
-        self.B_y = np.random.uniform(size=output_size)
-        self.W_hx = np.random.uniform(size=(input_size + hidden_size, hidden_size))
-        self.B_hx = np.random.uniform(size=hidden_size)
-
-        self._optimizer = None
+        self.h_t = None
+        self.prev_h_t = None
 
     def forward(self, input_tensor):
         batch_size = input_tensor.shape[0]
-        output_tensor = np.zeros((batch_size, self.hidden_size))
-        if self.memorize is False:
-            self.hidden_state = np.zeros(self.hidden_size)
-        for b in range(batch_size):
-            x_hat = np.hstack((self.hidden_state, input_tensor[b]))
-            self.hidden_state = np.tanh(np.dpt(x_hat, self.W_hx) + self.B_hx)  # ht
-            y = np.dot(self.hidden_state, self.W_y) + self.B_y
-            output_tensor[b] = 1 / (1 + np.exp(-y))
-
-        return output_tensor
-
-    def initialize(self, weights_initializer, bias_initializer):
-        self.W_y = weights_initializer.initialize(self.W_y.shape, self.hidden_size, self.output_size)
-        self.B_y = bias_initializer.initialize(self.B_y.shape, self.hidden_size, self.output_size)
-        self.W_hx = weights_initializer.initialize(self.W_hx.shape, self.W_hx.shape[0], self.hidden_size)
-        self.B_hx = bias_initializer.initialize(self.B_hx.shape, self.W_hx.shape[0], self.hidden_size)
-
-    @property
-    def calculate_regularization_loss(self):
-        return self.optimizer.regularizer.norm(self.)
-
-    @property
-    def optimizer(self):
-        return self._optimizer
-
-    @optimizer.setter
-    def optimizer(self, _opt):
-        self._optimizer = _opt
-        # self._optimizer.weights = copy.deepcopy(_opt)
-        # self._optimizer.bias = copy.deepcopy(_opt)
-
-    @property
-    def memorize(self):
-        return self.memorize
-
-    @memorize.setter
-    def memorize(self, x):
-        self.memorize = x
+        if self._memorize and self.h_t:
+            self.h_t[0] = self.prev_h_t
+        else:
+            self.h_t = np.zeros((batch_size+1, self.hidden_size))
+        y_t = np.zeros((batch_size, self.output_size))
