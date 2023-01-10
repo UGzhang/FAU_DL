9c96bc7bf69e37974cda87afc5acf675f5f48a5c
youyizhang <aaron.zhang.edu@outlook.com>
2023-01-10 22:49:55 +0800

    fit unfinish

--- exercise4_material/src_to_implement/trainer.py
+++ exercise4_material/src_to_implement/trainer.py
@@ -57,16 +57,21 @@ class Trainer:
         # -compute gradient by backward propagation
         # -update weights
         # -return the loss
-        #TODO
-        
+        self._optim.zero_grad()
+        pred = self._model(x)
+        loss = self._crit(pred, y)
+        loss.backward()
+        self._optim.step()
+        return loss
         
     
     def val_test_step(self, x, y):
-        
         # predict
         # propagate through the network and calculate the loss and predictions
         # return the loss and the predictions
-        #TODO
+        pred = self._model(x)
+        loss = self._crit(pred, y)
+        return loss, pred
         
     def train_epoch(self):
         # set training mode
@@ -74,8 +79,15 @@ class Trainer:
         # transfer the batch to "cuda()" -> the gpu if a gpu is given
         # perform a training step
         # calculate the average loss for the epoch and return it
-        #TODO
-    
+        self._model = self._model.train()
+        loss = 0
+        for x, y in tqdm(self._train_dl):
+            if self._cuda:
+                x = x.cuda()
+                y = y.cuda()
+            loss += self.train_step(x, y)
+            return loss / len(self._train_dl)
+
     def val_test(self):
         # set eval mode. Some layers have different behaviors during training and testing (for example: Dropout, BatchNorm, etc.). To handle those properly, you'd want to call model.eval()
         # disable gradient computation. Since you don't need to update the weights during testing, gradients aren't required anymore. 
@@ -85,23 +97,44 @@ class Trainer:
         # save the predictions and the labels for each batch
         # calculate the average loss and average metrics of your choice. You might want to calculate these metrics in designated functions
         # return the loss and print the calculated metrics
-        #TODO
-        
-    
+        loss = 0
+        pred_labels = []
+        y_labels = []
+        self._model = self._model.eval()
+        with t.no_grad():
+            for x, y in tqdm(self._val_test_dl):
+                if self._cuda:
+                    x = x.cuda()
+                    y = y.cuda()
+                batch_loss, pred = self.val_test_step(x, y)
+                pred_labels.append(pred)
+                y_labels.append(y)
+                loss += batch_loss
+        return loss / len(self._val_test_dl)
+
+
     def fit(self, epochs=-1):
         assert self._early_stopping_patience > 0 or epochs > 0
         # create a list for the train and validation losses, and create a counter for the epoch 
-        #TODO
-        
+        train_loss = []
+        val_loss = []
+        epoch = 0
         while True:
-      
             # stop by epoch number
             # train for a epoch and then calculate the loss and metrics on the validation set
             # append the losses to the respective lists
             # use the save_checkpoint function to save the model (can be restricted to epochs with improvement)
             # check whether early stopping should be performed using the early stopping criterion and stop if so
             # return the losses for both training and validation
-        #TODO
+            if epoch == epochs:
+                break
+            train_loss.append(self.train_epoch())
+            val_loss.append(self.val_test())
+            self.save_checkpoint(epoch)
+            if 
+
+
+
                     
         
         
